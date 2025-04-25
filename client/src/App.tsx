61c851283a9663cd4c2d70ec532533881eca977f
My Ummah Apps <39090870+TheFlyingDonut@users.noreply.github.com>
2025-04-25 17:22:05 +0100

    style: Add placeholder text when table contains no tasks, other minor styling improvements to table and create task form element

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -89,7 +89,7 @@ function App() {
   };
 
   const inputStyles =
-    "py-2 pl-3 mb-4 transition border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200";
+    "border border-gray-500 focus:border-gray-900 py-2 pl-3 mb-4 transition border border-gray-300 rounded-md";
 
   return (
     <section className="flex flex-col justify-center items-center text-[#e4edfc] mt-[10vh]">
@@ -102,7 +102,7 @@ function App() {
               editTask(taskId, e);
             }
           }}
-          className="absolute text-[#3b3b3b] w-1/2 p-10 transform -translate-x-1/2 -translate-y-1/2 bg-[#cbcbcb] top-1/2 left-1/2 rounded-2xl"
+          className="z-10 absolute text-[#3b3b3b] w-2/3 p-10 transform -translate-x-1/2 -translate-y-1/2 bg-[#cbcbcb] top-1/2 left-1/2 rounded-2xl"
         >
           <div className="flex flex-col text-sm">
             <label className="mb-1" htmlFor="title">
@@ -166,6 +166,8 @@ function App() {
                 onChange={handleFormInputs}
                 value={formData.due}
                 placeholder="Due Date/Time"
+                // min="1950-01-01"
+                min={new Date().toISOString().split("T")[0]}
                 required
               />
             </div>
@@ -186,7 +188,7 @@ function App() {
 
       <section className="flex flex-col w-10/12 max-w-3xl">
         <button
-          className="self-end w-1/4 p-2 mt-2 text-white bg-blue-600 rounded-lg"
+          className="self-end w-1/4 p-2 mt-2 text-white bg-blue-700 rounded-lg"
           onClick={() => {
             setFormVisibility(true);
           }}
@@ -194,7 +196,7 @@ function App() {
           Create Task
         </button>
         <section className="overflow-x-auto">
-          <table className="mt-5 text-center">
+          <table className="w-full mt-5 text-center table-fixed ">
             <thead>
               <tr className="text-sm">
                 <th className="w-1/6">TITLE</th>
@@ -205,62 +207,72 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {tasksList.map((task) => {
-                return (
-                  <tr className="border-b border-stone-700" key={task.id}>
-                    <td className="">{task.title}</td>
-                    <td>{task.description}</td>
-                    <td>
-                      <p
-                        className={`p-1 rounded-md whitespace-nowrap ${
-                          task.status === "Complete"
-                            ? "bg-green-200 text-green-800"
-                            : task.status === "Not Started"
-                            ? "bg-yellow-200 text-yellow-800"
-                            : task.status === "In Progress"
-                            ? "bg-amber-200 text-amber-800"
-                            : "bg-transparent"
-                        }`}
-                      >
-                        {task.status}
-                      </p>
-                    </td>
-                    <td className="rounded-md">
-                      {new Date(task.due).toLocaleString().replace(",", "")}
-                    </td>
-                    <td>
-                      <i
-                        role="button"
-                        aria-label="Edit Task"
-                        tabIndex={0}
-                        onClick={() => {
-                          setFormData({
-                            id: task.id,
-                            title: task.title,
-                            description: task.description,
-                            status: task.status,
-                            due: task.due,
-                          });
-                          setFormVisibility(true);
-                          setIsEditing(true);
-                          setTaskId(task.id);
-                        }}
-                        className="pr-5 fa-solid fa-pen"
-                      ></i>
-                      <i
-                        role="button"
-                        aria-label="Delete Task"
-                        tabIndex={0}
-                        onClick={() => {
-                          const userChoice = confirm("Delete Task?");
-                          if (userChoice) deleteTask(task.id);
-                        }}
-                        className="fa-solid fa-trash"
-                      ></i>
-                    </td>
-                  </tr>
-                );
-              })}
+              {tasksList.length > 0 ? (
+                tasksList.map((task) => {
+                  return (
+                    <tr className="border-b border-stone-700" key={task.id}>
+                      <td className="overflow-hidden truncate whitespace-nowrap">
+                        {task.title}
+                      </td>
+                      <td className="overflow-hidden truncate whitespace-nowrap">
+                        {task.description}
+                      </td>
+                      <td className="overflow-hidden truncate whitespace-nowrap">
+                        <p
+                          className={`p-1 rounded-md whitespace-nowrap ${
+                            task.status === "Complete"
+                              ? "bg-green-200 text-green-800"
+                              : task.status === "Not Started"
+                              ? "bg-yellow-200 text-yellow-800"
+                              : task.status === "In Progress"
+                              ? "bg-orange-200 text-orange-800"
+                              : "bg-transparent"
+                          }`}
+                        >
+                          {task.status}
+                        </p>
+                      </td>
+                      <td className="rounded-md">
+                        {new Date(task.due).toLocaleString().replace(",", "")}
+                      </td>
+                      <td>
+                        <i
+                          role="button"
+                          aria-label="Edit Task"
+                          tabIndex={0}
+                          onClick={() => {
+                            setFormData({
+                              id: task.id,
+                              title: task.title,
+                              description: task.description,
+                              status: task.status,
+                              due: task.due,
+                            });
+                            setFormVisibility(true);
+                            setIsEditing(true);
+                            setTaskId(task.id);
+                          }}
+                          className="pr-5 fa-solid fa-pen"
+                        ></i>
+                        <i
+                          role="button"
+                          aria-label="Delete Task"
+                          tabIndex={0}
+                          onClick={() => {
+                            const userChoice = confirm("Delete Task?");
+                            if (userChoice) deleteTask(task.id);
+                          }}
+                          className="fa-solid fa-trash"
+                        ></i>
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <p className="absolute text-2xl text-gray-500 -translate-x-1/2 left-1/2 top-1/2">
+                  You don’t have any tasks yet.
+                </p>
+              )}
             </tbody>
           </table>
         </section>
